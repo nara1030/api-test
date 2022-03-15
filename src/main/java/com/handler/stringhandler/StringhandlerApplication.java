64604bcc81acb8c4d35dc48c6266a606097e70ee
hjeom <nara1030@naver.com>
2022-03-15 16:00:54 +0900

    setting: 프로젝트 생성

---
+++ src/main/java/com/handler/stringhandler/StringhandlerApplication.java
@@ -0,0 +1,13 @@
+package com.handler.stringhandler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StringhandlerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StringhandlerApplication.class, args);
+    }
+
+}
